994c2980bfef92cb0f8c89499f9a4bdce7b7ea94
sarocha-chang <sarocha_chang@cmu.ac.th>
2022-05-16 22:40:34 +0700

    Add data of user and post before change data that show by mock to be data from json

--- lib/util/user.dart
+++ lib/util/user.dart
@@ -3,18 +3,18 @@ List users = [
     "userId": "1",
     "username": "Jhon",
     "password": "12345",
-    "profile": "assets/images/profile.jpg",
+    "profile": "assets/images/profile/cat.jpg",
   },
   {
     "userId": "2",
     "username": "Alfred",
     "password": "12345",
-    "profile": "assets/images/profile1.jpg",
+    "profile": "assets/images/profile/dog.jpg",
   },
   {
     "userId": "3",
     "username": "Alice",
     "password": "12345",
-    "profile": "assets/images/profile2.jpg",
+    "profile": "assets/images/profile/rabbit.jpg",
   }
 ];
